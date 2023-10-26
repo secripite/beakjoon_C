148544badcee32d2bc6d287810178e6cef218735
secripite <keonho04@gmail.com>
2023-10-26 19:26:05 +0900

    2579

---
+++ C/beakjoon/2579.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int temp;
+    int sum = 0;
+    int min = 100;
+    int flag = 0;
+
+    for (int i = 0; i < 7; i++){
+        scanf("%d", &temp);
+        if(temp % 2 == 1){
+            if(temp < min){
+                min = temp;
+            }
+            sum += temp;
+            flag = 1;
+        }
+    }
+
+    if(flag == 1){
+        printf("%d\n", sum);
+        printf("%d\n", min);
+    }
+    else{
+        printf("-1");
+    }
+}
